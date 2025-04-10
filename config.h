2d337fbcf82b6a229558193843fb41425202a5b7
GamerHammer2K4F647Ultimate <paprikaskrumpli122@gmail.com>
2025-04-10 21:53:39 +0200

    change font back to mono

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka-Regular:pixelsize=14:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
